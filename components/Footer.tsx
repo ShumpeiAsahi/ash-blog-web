a9a1fa94569a3ea08e2a78c87ee10c4f6cbb37a9
ShumpeiAsahi <asahishumpei@gmail.com>
2023-01-13 18:14:44 +0900

    edit copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
     return (
       <>
       <div className='flex items-center flex-wrap bg-white p-3 justify-center'>
-        <p className='text-xs text-stone-900 tracking-wide'>Copyright 2022 Shumpei Asahi.</p>
+        <p className='text-xs text-stone-900 tracking-wide'>Copyright 2023 Shumpei Asahi.</p>
       </div>
       </>
     );
